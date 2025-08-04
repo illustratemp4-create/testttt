2ee0e9ab50eda1638f85f3e7636da2bc61eeec0a
Rohit <rohitrajagopal04@gmail.com>
2025-08-04 18:32:40 +0530

    local vectoring working fr fr

--- query.py
+++ query.py
@@ -1,5 +1,4 @@
-from sentence_transformers import SentenceTransformer
-from pinecone import Pinecone
+from pinecone_db import embed_chunks, search
 from dotenv import load_dotenv
 import os
 from groq import Groq
@@ -7,40 +6,19 @@ from groq import Groq
 load_dotenv()
 
 
-def query_llm(queries):
-    pc = Pinecone(api_key=os.environ['PINECONE_API_KEY'])
-    index = pc.Index(host=os.environ['PINECONE_HOST'])
-
+def query_llm(json_chunks, queries):
     client = Groq(
         api_key=os.environ['GROQ_API_KEY'],
     )
 
-    model = SentenceTransformer('BAAI/bge-base-en-v1.5')
-
-    # queries = [
-    #         "What is the grace period for premium payment under the National Parivar Mediclaim Plus Policy?",
-    #         "What is the waiting period for pre-existing diseases (PED) to be covered?",
-    #         "Does this policy cover maternity expenses, and what are the conditions?",
-    #         "What is the waiting period for cataract surgery?",
-    #         "Are the medical expenses for an organ donor covered under this policy?",
-    #         "What is the No Claim Discount (NCD) offered in this policy?",
-    #         "Is there a benefit for preventive health check-ups?",
-    #         "How does the policy define a 'Hospital'?",
-    #         "What is the extent of coverage for AYUSH treatments?",
-    #         "Are there any sub-limits on room rent and ICU charges for Plan A?"
-    #     ]
+    chunks, embeddings = embed_chunks(json_chunks)
 
     answers = []
 
     for query in queries:
-        query_embed = model.encode(query).tolist()
+        top_chunks = search(query, chunks, embeddings)
 
-        res = index.query(vector=query_embed, top_k=5, include_metadata=True, namespace="Insurance")
-        # print(res)
-        docs = [match['metadata']['text'] for match in res['matches']]
-        # print(docs)
-
-        context = '\n\n'.join(docs)
+        context = '\n\n'.join(top_chunks)
 
         prompt = f"""You are an expert legal assistant.
         Use the following context to answer the question.
@@ -64,10 +42,6 @@ def query_llm(queries):
             model="llama-3.3-70b-versatile",
         )
         answers.append(chat_completion.choices[0].message.content)
-
-    # print("ANSWERS:")
-    # for i in range(len(answers)):
-    #     print(f"\n{i + 1}. {answers[i]}")
 
     ans = {"answers": answers}
 
