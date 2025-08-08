2eb31e6290d9156c95b0a71becd1e2327e996eb2
Rohit <rohitrajagopal04@gmail.com>
2025-08-08 11:26:44 +0530

    Revert "modified"
    This reverts commit 1d7468f773d739003b1e79ba354df54a9a31ef45.

--- query.py
+++ query.py
@@ -20,29 +20,17 @@ def query_llm(json_chunks, queries):
 
         context = '\n\n'.join(top_chunks)
 
-        prompt = f"""
-        You are an expert insurance policy assistant.
-
-        Use ONLY the information provided in the context below to answer the question accurately and concisely.
-
-        RULES:
-        - Do NOT use any external knowledge.
-        - If the answer is not clearly stated in the context, respond with: "Not found in document".
-        - Quote exact limits or durations when available.
-        - Do NOT include any escape characters, backslashes, or formatting artifacts.
-        - Avoid overly long answers — keep it short and factual (ideally under 3 sentences).
-        - Do not explain the clause numbers or legal citations unless necessary for clarity.
-        - Use a clear, formal, and factual tone (avoid legalese unless quoting a definition).
-        - Remove any unnecessary slash-n's and slashes that are not part of the answer.
-
+        prompt = f"""You are an expert legal assistant.
+        Use the following context to answer the question.
+        If it's not answerable from the text, say "Not found in document"
+        
         Context:
         {context}
-
+        
         Question:
         {query}
-
-        Answer:
-        """
+        
+        Answer: """
 
         chat_completion = client.chat.completions.create(
             messages=[
@@ -51,10 +39,10 @@ def query_llm(json_chunks, queries):
                     "content": f"{prompt}",
                 }
             ],
-            model="meta-llama/llama-4-scout-17b-16e-instruct",
+            model="llama-3.3-70b-versatile",
         )
         answers.append(chat_completion.choices[0].message.content)
-    cleaned_answers = [ans.replace('\n', ' ').replace('\r', ' ').strip() for ans in answers]
-    ans = {"answers": cleaned_answers}
+
+    ans = {"answers": answers}
 
     return ans
