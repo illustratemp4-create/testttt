8ec79878007953d768308b0c1c3bfa3ecb599971
Rohit <rohitrajagopal04@gmail.com>
2025-08-08 19:34:05 +0530

    fixed stripping

--- query.py
+++ query.py
@@ -41,10 +41,15 @@ def query_llm(json_chunks, queries):
 
         context += '\n\n'.join(top_chunks)
 
+    for query in queries:
+        top_chunks = search(query, chunks, embeddings)
+
+        context += '\n\n'.join(top_chunks)
+
     prompt = f"""You are an expert legal assistant.
     Use the following context to answer the following questions.
     Separate each answer with a '|'. Do not repeat the questions in the answer.
-    If it's not answerable from the text, say "Not found in document"
+    If it's not answerable from the text, say "Not found in document". If conditions apply for the answer, specify them.
     
     Context:
     {context}
@@ -64,10 +69,11 @@ def query_llm(json_chunks, queries):
         model="meta-llama/llama-4-scout-17b-16e-instruct",
     )
     answers = chat_completion.choices[0].message.content
+    print(answers)
 
     # Postprocessing
     answers = answers.split('|')
-    answers = [answer.strip() for answer in answers if answer != ""]
+    answers = [answer.strip() for answer in answers if answer.strip() != ""]
 
     ans = {"answers": answers}
 
