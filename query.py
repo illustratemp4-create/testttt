b42197ff7d80356f9561da3cc4262f9c436ba778
Rohit <rohitrajagopal04@gmail.com>
2025-08-08 16:28:53 +0530

    batch query version

--- query.py
+++ query.py
@@ -13,35 +13,40 @@ def query_llm(json_chunks, queries):
 
     chunks, embeddings = embed_chunks(json_chunks)
 
-    answers = []
-
+    # Batch query to LLM
+    context = ''
     for query in queries:
         top_chunks = search(query, chunks, embeddings)
 
-        context = '\n\n'.join(top_chunks)
+        context += '\n\n'.join(top_chunks)
 
-        prompt = f"""You are an expert legal assistant.
-        Use the following context to answer the question.
-        If it's not answerable from the text, say "Not found in document"
-        
-        Context:
-        {context}
-        
-        Question:
-        {query}
-        
-        Answer: """
+    prompt = f"""You are an expert legal assistant.
+    Use the following context to answer the following questions.
+    Separate each answer with a '|'. Do not repeat the questions in the answer.
+    If it's not answerable from the text, say "Not found in document"
+    
+    Context:
+    {context}
+    
+    Question:
+    {queries}
+    
+    Answer: """
 
-        chat_completion = client.chat.completions.create(
-            messages=[
-                {
-                    "role": "user",
-                    "content": f"{prompt}",
-                }
-            ],
-            model="llama-3.3-70b-versatile",
-        )
-        answers.append(chat_completion.choices[0].message.content)
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": f"{prompt}",
+            }
+        ],
+        model="meta-llama/llama-4-scout-17b-16e-instruct",
+    )
+    answers = chat_completion.choices[0].message.content
+
+    # Postprocessing
+    answers = answers.split('|')
+    answers = [answer.strip() for answer in answers if answer]
 
     ans = {"answers": answers}
 
