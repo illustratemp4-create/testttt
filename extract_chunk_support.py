4d58a3dea80181cb8a7cd846e75c96d80600acce
Rohit <rohitrajagopal04@gmail.com>
2025-08-10 20:05:20 +0530

    added better chunking

--- extract_chunk_support.py
+++ extract_chunk_support.py
@@ -1,6 +1,7 @@
 import fitz
 import requests
 from io import BytesIO
+import re
 
 
 def download_and_extract(url):
@@ -15,5 +16,24 @@ def download_and_extract(url):
     return full_text
 
 
-def chunk_text(text, chunk_size=500):
-    return [text[i:i+chunk_size] for i in range(0, len(text), chunk_size)]
+def chunk_text(text, chunk_size=500, overlap=50):
+    sentences = re.split(r'(?<=[.?!])\s+', text)
+    chunks = []
+    current_chunk = ""
+
+    for sentence in sentences:
+        if len(current_chunk) + len(sentence) + 1 > chunk_size:
+            chunks.append(current_chunk.strip())
+            # Start new chunk with overlap from the end of the last chunk
+            if overlap > 0 and chunks[-1]:
+                overlap_text = chunks[-1][-overlap:]
+                current_chunk = overlap_text + " " + sentence
+            else:
+                current_chunk = sentence
+        else:
+            current_chunk += " " + sentence
+
+    if current_chunk.strip():
+        chunks.append(current_chunk.strip())
+
+    return chunks
